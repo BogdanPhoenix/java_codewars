d892904bf4e1bf5ece970d4da8f1709adb3a47fe
BogdanPhoenix <bohdan.musco@gmail.com>
2024-08-29 19:12:47 +0300

    Implementation task 23 (Long variant)

--- src/main/java/org/example/tasks21_30/task23/Snail.java
+++ src/main/java/org/example/tasks21_30/task23/Snail.java
@@ -1,5 +1,8 @@
 package org.example.tasks21_30.task23;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snail {
 
     private Snail() {}
@@ -9,33 +12,94 @@ public class Snail {
             return new int[0];
         }
 
-        int[] result = new int[array.length * array[0].length];
-        int index = 0;
-        int top = 0;
-        int left = 0;
-        int bottom = array.length - 1;
-        int right = array[0].length - 1;
+        List<Integer> result = new ArrayList<>();
+        MatrixInfo matrixInfo = new MatrixInfo(array);
 
-        while (index < result.length) {
-            for(int i = left; i <= right; i++) {
-                result[index++] = array[top][i];
+        while (matrixInfo.top <= matrixInfo.bottom && matrixInfo.left <= matrixInfo.right) {
+            traverseRight(array, result, matrixInfo);
+            matrixInfo.nextTop();
+
+            traverseDown(array, result, matrixInfo);
+            matrixInfo.prevRight();
+
+            if (matrixInfo.isTraverseLeft()) {
+                traverseLeft(array, result, matrixInfo);
+                matrixInfo.prevBottom();
             }
-            ++top;
-            for(int i = top; i <= bottom; i++) {
-                result[index++] = array[i][right];
+
+            if (matrixInfo.isTraverseTop()) {
+                traverseTop(array, result, matrixInfo);
+                matrixInfo.nextLeft();
             }
-            --right;
-            for(int i = right; i >= left; i--) {
-                result[index++] = array[bottom][i];
-            }
-            --bottom;
-            for(int i = bottom; i >= top; i--) {
-                result[index++] = array[i][left];
-            }
-            ++left;
         }
 
-        return result;
+        return result.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+
+    private static void traverseRight(int[][] array, List<Integer> result, MatrixInfo matrixInfo) {
+        for(int i = matrixInfo.left; i <= matrixInfo.right; i++) {
+            result.add(array[matrixInfo.top][i]);
+        }
+    }
+
+    private static void traverseDown(int[][] array, List<Integer> result, MatrixInfo matrixInfo) {
+        for (int i = matrixInfo.top; i <= matrixInfo.bottom; i++) {
+            result.add(array[i][matrixInfo.right]);
+        }
+    }
+
+    private static void traverseLeft(int[][] array, List<Integer> result, MatrixInfo matrixInfo) {
+        for (int i = matrixInfo.right; i >= matrixInfo.left; i--) {
+            result.add(array[matrixInfo.bottom][i]);
+        }
+    }
+
+    private static void traverseTop(int[][] array, List<Integer> result, MatrixInfo matrixInfo) {
+        for (int i = matrixInfo.bottom; i >= matrixInfo.top; i--) {
+            result.add(array[i][matrixInfo.left]);
+        }
+    }
+
+    private static class MatrixInfo {
+
+        int top;
+        int left;
+        int bottom;
+        int right;
+
+        MatrixInfo(int[][] array) {
+            this.top = 0;
+            this.left = 0;
+            this.bottom = array.length - 1;
+            this.right = array[0].length - 1;
+        }
+
+        void nextTop() {
+            this.top++;
+        }
+
+        void prevBottom() {
+            this.bottom--;
+        }
+
+        void nextLeft() {
+            this.left++;
+        }
+
+        void prevRight() {
+            this.right--;
+        }
+
+        boolean isTraverseLeft() {
+            return this.top <= this.bottom;
+        }
+
+        boolean isTraverseTop() {
+            return this.left <= this.right;
+        }
+
     }
 
 }
